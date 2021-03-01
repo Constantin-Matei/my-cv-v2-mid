e3653df87fa61e788c80b9e08116c5dff538641b
Constantin Matei <constantin@nepretuit.ro>
2021-03-01 14:15:42 +0000

    Work in progress on welcome section

--- src/component/welcome/welcome.component.jsx
+++ src/component/welcome/welcome.component.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../header'
 import * as WS from './welcome.style'
 
-export const Welcome = ({name, jobName}) => {
+export const Welcome = () => {
+  const [ data, salveazaData ] = useState({})
+  useEffect(() => {
+    // vreau sa iau datele de pe server in frontend
+    fetch('http://localhost:3800/welcome')
+      .then(response=> response.json())
+      .then(response=>{
+        // console.log(response)
+        salveazaData(response)
+      })
+  }, [])
+  console.log(data)
+  const { title, name, jobName, descriptionTitle } = data
   return (
     <WS.Wrapper>
-      <Header title='Welcome to My CV'/>
-      <WS.Name>Constantin Matei</WS.Name>
-      <WS.JobName>Early Javascript Junior</WS.JobName>
-      <WS.DescriptionTitle>About me</WS.DescriptionTitle>
+      <Header title={title}/>
+      <WS.Name>{name}</WS.Name>
+      <WS.JobName>{jobName}</WS.JobName>
+      <WS.DescriptionTitle>{descriptionTitle}</WS.DescriptionTitle>
     </WS.Wrapper>
   )
 }
